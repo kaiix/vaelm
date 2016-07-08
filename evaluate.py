4fa4041bada6be05ca609b42b0d89049aa07fe35
kaiix <kvn.hou@gmail.com>
2016-07-08 12:16:53 +0800

    Fix evaluation script

--- evaluate.py
+++ evaluate.py
@@ -1,10 +1,9 @@
-"""Semantic relatedness prediction using LSTMs."""
 from __future__ import absolute_import
 from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import division
 
-from lstm_sim import evaluate
+from vaelm import evaluate
 
 
 if __name__ == '__main__':
